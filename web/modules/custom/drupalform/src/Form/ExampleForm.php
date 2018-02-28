9c9bbd73f38d19cca787786cef17e8b9b6628f01
hamdle <contact@ericmarty.co>
2018-02-27 21:10:33 -0800

    Build form

--- web/modules/custom/drupalform/src/Form/ExampleForm.php
+++ web/modules/custom/drupalform/src/Form/ExampleForm.php
@@ -6,5 +6,53 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 class ExampleForm extends FormBase {
-    
+
+    /**
+     * Returns a unique string identifying the form.
+     *
+     * @return string
+     *   The unique string identifying the form.
+     */
+    public function getFormId()
+    {
+        // TODO: Implement getFormId() method.
+    }
+
+    /**
+     * Form constructor.
+     *
+     * @param array $form
+     *   An associative array containing the structure of the form.
+     * @param \Drupal\Core\Form\FormStateInterface $form_state
+     *   The current state of the form.
+     *
+     * @return array
+     *   The form structure.
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $form['cat_name'] = [
+            '#type' => 'textfield',
+            '#title' => 'Cat Name',
+        ];
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('OK'),
+        ];
+
+        return $form;
+    }
+
+    /**
+     * Form submission handler.
+     *
+     * @param array $form
+     *   An associative array containing the structure of the form.
+     * @param \Drupal\Core\Form\FormStateInterface $form_state
+     *   The current state of the form.
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement submitForm() method.
+    }
 }
