35b2020cb5e878f69a5a72da4e9bbeb11d6dd24b
hamdle <contact@ericmarty.co>
2018-03-16 16:03:27 -0700

    Start work on plugin chapter using pluginmodule

---
+++ web/modules/custom/pluginmodule/src/Plugin/Block/Copyright.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\pluginmodule\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * @Block(
+ *  id = "copyright_block",
+ *  admin_label = @Translation("Copyright"),
+ *  category = @Translation("Custom")
+ * )
+ */
+class Copyright extends BlockBase {
+
+}
