e3f78d87a48a5c4d83ed2ad5fe06265d29aa1eb2
hamdle <contact@ericmarty.co>
2018-03-18 22:12:45 -0700

    Fix real name field type bugs

--- web/modules/custom/realname/src/Plugin/Field/FieldType/RealName.php
+++ web/modules/custom/realname/src/Plugin/Field/FieldType/RealName.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\realname\Plugin\Field\FieldType;
 
-use Drupal\Core\Field\FieldType;
+use Drupal\Core\Field\FieldItemBase;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\TypedData\DataDefinition;
 
@@ -10,38 +10,36 @@ use Drupal\Core\TypedData\DataDefinition;
  * Plugin implementation of the 'realname' field type.
  *
  * @FieldType(
- *      id = "realname",
- *      label = @Translation("Real name"),
- *      description = @Translation("This field stores a first and last name."),
- *      category = @Translation("General"),
- *      default_widget = "string_textfield",
- *      default_formatter = "string",
+ *   id = "realname",
+ *   label = @Translation("Real name"),
+ *   description = @Translation("This field stores a first and last name."),
+ *   category = @Translation("General"),
+ *   default_widget = "string_textfield",
+ *   default_formatter = "string"
+ * )
  */
-
-class RealName extends FieldItemBase
-{
+class RealName extends FieldItemBase {
 
     /**
      * {@inheritdoc}
-    */
-    public static function schema(\Drupal\Core\Field\FieldStorageDefinitionInterface $field_definition)
-    {
+     */
+    public static function schema(\Drupal\Core\Field\FieldStorageDefinitionInterface $field_definition) {
         return [
             'columns' => [
                 'first_name' => [
-                    'description' => 'First name',
+                    'description' => 'First name.',
                     'type' => 'varchar',
                     'length' => '255',
                     'not null' => TRUE,
                     'default' => '',
                 ],
                 'last_name' => [
-                    'description' => 'Last name',
+                    'description' => 'Last name.',
                     'type' => 'varchar',
                     'length' => '255',
                     'not null' => TRUE,
                     'default' => '',
-                ]
+                ],
             ],
             'indexes' => [
                 'first_name' => ['first_name'],
@@ -51,11 +49,13 @@ class RealName extends FieldItemBase
     }
 
     /**
-     * @{inheritdoc}
+     * {@inheritdoc}
      */
-    public static function propertyDefinition(\Drupal\Core\Field\FieldStorageDefinitionInterface $field_definition)
-    {
-        $properties['first_name'] = \Drupal\Core\TypedData\DataDefinition::create('string')->setLabel(t('First name'));
-        $properties['last_name'] = \Drupal\Core\TypedData\DataDefinition::create('string')->setLabel(t('Last name'));
+    public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+        $properties['first_name'] = DataDefinition::create('string')->setLabel(t('First name'));
+        $properties['last_name'] = DataDefinition::create('string')->setLabel(t('Last name'));
+
+        return $properties;
     }
+
 }
