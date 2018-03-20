52b0e168cb956a5de93d591db8944ff598fe6b60
hamdle <contact@ericmarty.co>
2018-03-20 10:34:53 -0700

    Annotate field widget and implement interface method

--- web/modules/custom/realname/src/Plugin/Field/FieldWidget/RealNameDefaultWidget.php
+++ web/modules/custom/realname/src/Plugin/Field/FieldWidget/RealNameDefaultWidget.php
@@ -2,8 +2,25 @@
 
 namespace Drupal\realname\Plugin\Field\FieldWidget;
 
+use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * @FieldWidget(
+ *   id = "realname_default",
+ *   label = @Translation("Real nom"),
+ *   field_types = {
+ *     "realname"
+ *   }
+ * )
+ */
 class RealNameDefaultWidget extends WidgetBase {
-    
+    /**
+     * {@inheritdoc}
+     */
+    public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement formElement() method.
+    }
 }
