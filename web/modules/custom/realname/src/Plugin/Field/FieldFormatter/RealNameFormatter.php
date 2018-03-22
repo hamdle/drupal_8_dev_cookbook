18aaa203c22cadc8f55d93317e407c18b81cbc4d
hamdle <contact@ericmarty.co>
2018-03-22 10:54:51 -0700

    Field formatter annotation

--- web/modules/custom/realname/src/Plugin/Field/FieldFormatter/RealNameFormatter.php
+++ web/modules/custom/realname/src/Plugin/Field/FieldFormatter/RealNameFormatter.php
@@ -5,6 +5,17 @@ namespace Drupal\realname\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 
+/**
+ * plugin implementation of the 'realname_one_line' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "realname_one_line",
+ *   label = @Translation("Real name (one line)"),
+ *   field_types = {
+ *     "realname"
+ *   }
+ * )
+ */
 class RealNameFormatter extends FormatterBase {
     // TODO : implement field item list interface
 }
