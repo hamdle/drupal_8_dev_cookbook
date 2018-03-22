93b5b865eee8025035be7f9916769347591bc5a5
hamdle <contact@ericmarty.co>
2018-03-22 10:51:00 -0700

    Start field formatter for real name

---
+++ web/modules/custom/realname/src/Plugin/Field/FieldFormatter/RealNameFormatter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\realname\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+class RealNameFormatter extends FormatterBase {
+    // TODO : implement field item list interface
+}
