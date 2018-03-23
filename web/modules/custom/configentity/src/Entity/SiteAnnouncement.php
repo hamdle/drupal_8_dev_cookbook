20a737521542be5fe5b9ddda71a2789ad7ee2906
hamdle <contact@ericmarty.co>
2018-03-22 17:43:12 -0700

    Set handlers and routes using config entity annotation

--- web/modules/custom/configentity/src/Entity/SiteAnnouncement.php
+++ web/modules/custom/configentity/src/Entity/SiteAnnouncement.php
@@ -8,11 +8,25 @@ use Drupal\Core\Config\Entity\ConfigEntityBase;
  * @ConfigEntityType(
  *   id = "announcement",
  *   label = @Translation("Site Announcement"),
+ *   handlers = {
+ *      "list_builder" = "Drupal\configentity\SiteAnnouncementListBuilder",
+ *      "form" = {
+ *        "default" = "Drupal\configentity\SiteAnnouncementForm",
+ *        "add" = "Drupal\configentity\SiteAnnouncementForm",
+ *        "edit" = "Drupal\configentity\SiteAnnouncementForm",
+ *        "delete" = "Drupal\Core\Entity\EntityDeleteForm",
+ *      }
+ *   },
  *   config_prefix = "announcement",
  *   entity_keys = {
  *     "id" = "id",
  *     "label" = "label",
  *   }
+ *   links = {
+ *     "delete-form" = "/admin/config/system/site-announcements/manage/{announcement}/delete",
+ *     "edit-form" = "/admin/config/system/site-announcements/manage/{announcement}",
+ *     "collection" = "/admin/config/system/site-announcements",
+ *   },
  *   config_export = {
  *     "id",
  *     "label",
