8409d0e5480e73158c722368a9823cadc646626e
hamdle <contact@ericmarty.co>
2018-05-14 15:15:33 -0700

    Create block to add angular code

---
+++ web/modules/custom/angularmodule/src/Plugin/Block/AngularBlock.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\angularmodule\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a block for AngularJS example.
+ *
+ * @Block(
+ *   id = "angularmodule_agular_block",
+ *   admin_label = @Translation("AngularJS Block")
+ * )
+ */
+class AngularBlock extends BlockBase {
+	public function build() {
+		// TODO generate and return block code
+	}
+}
+
